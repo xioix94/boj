53bc1d7bc067245def9c081474e71a5946ba1d30
xioix94 <53420414+xioix94@users.noreply.github.com>
2025-02-27 15:20:44 +0900

    [level 3] Title: 가장 먼 노드, Time: 49.25 ms, Memory: 114 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -16,38 +16,38 @@ class Solution {
         // 발견한 노드 체크
         int[] discovered = new int[n + 1];
         
-        // 가장 멀리 떨어진 노드의 개수
-        int[] farthestNodeCount = new int[n + 1];
-        Arrays.fill(farthestNodeCount, -1);
-        farthestNodeCount[1] = 0;
+        // 거리 리스트
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, -1);
+        dist[1] = 0;
         
         // bfs
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
         discovered[1] = 1;
         while(!queue.isEmpty()) {
-            int next = queue.remove();
-            for (int i = 1; i < n + 1; i++) {
-                if (adjLinkedList.get(next).contains(i) && discovered[i] == 0) {
-                    queue.add(i);
-                    discovered[i] = 1;
-                    farthestNodeCount[i] = farthestNodeCount[next] + 1;
+            int curr = queue.remove();
+            for (int neighbor : adjLinkedList.get(curr)) {
+                if (discovered[neighbor] == 0) {
+                    queue.add(neighbor);
+                    discovered[neighbor] = 1;
+                    dist[neighbor] = dist[curr] + 1;
                 }
             }
         }
         
         // 방문한 노드 중에 제일 큰 수의 방문 수 반환
         int maxValue = -1;
-        for (int i = 1; i < farthestNodeCount.length; i++) {
-            if (farthestNodeCount[i] > maxValue) {
-                maxValue = farthestNodeCount[i];
+        for (int i = 1; i < dist.length; i++) {
+            if (dist[i] > maxValue) {
+                maxValue = dist[i];
             }
         }
         
         // 제일 큰 수에 해당하는 개수 반환
         int result = 0;
-        for (int i = 1; i < farthestNodeCount.length; i++) {
-            if (farthestNodeCount[i] == maxValue) {
+        for (int i = 1; i < dist.length; i++) {
+            if (dist[i] == maxValue) {
                 result++;
             }
         }
