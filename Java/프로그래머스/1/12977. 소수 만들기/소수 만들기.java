d1dbfbea139ca09c47a69ff17b43e87e7e32400d
xioix94 <53420414+xioix94@users.noreply.github.com>
2025-02-28 13:11:03 +0900

    [level 1] Title: 소수 만들기, Time: 21.89 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer += 1;
+                    }
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    public boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+        
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
