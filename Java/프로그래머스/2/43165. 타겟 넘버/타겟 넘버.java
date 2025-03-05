990f8b09a0097d8a63516b4651d66a8cf1020328
xioix94 <53420414+xioix94@users.noreply.github.com>
2025-03-05 17:08:35 +0900

    [level 2] Title: 타겟 넘버, Time: 7.56 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = calculate(numbers, target);
+        return answer;
+    }
+    
+    public int calculate(int[] numbers, int target) {
+        int count = 0;
+        return calculateRecursive(numbers, 0, target, count, 0);
+    }
+    
+    public int calculateRecursive(int[] numbers, int index, int target, int count, int sum) {
+        
+        // 마지막인 경우 종료
+        if (index == numbers.length - 1) {
+            
+            // 요소 더하는 경우
+            if (sum + numbers[index] == target) {
+                count++;
+            }
+
+            // 요소 빼는 경우
+            if (sum - numbers[index] == target) {
+                count++;
+            }
+            
+            return count;
+        }
+        
+        // 마지막 요소 아니라면 함수 재호출
+        count = calculateRecursive(numbers, index + 1, target, count, sum + numbers[index]);
+        count = calculateRecursive(numbers, index + 1, target, count, sum - numbers[index]);
+        
+        return count;
+    }
+}
