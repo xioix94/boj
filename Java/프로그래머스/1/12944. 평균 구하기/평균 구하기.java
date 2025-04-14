f478f843c6b8dff8fa4c7ae313ae06b64362018b
xioix94 <53420414+xioix94@users.noreply.github.com>
2025-04-14 11:59:36 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+        
+        for (int a : arr) {
+            sum += a;
+        }
+        
+        return (double) sum / arr.length;
+    }
+}
